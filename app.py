658bf913be4d9dd98d7aaba6a1ddd33b4c272ae8
lievan <0evanli0@gmail.com>
2022-11-29 18:46:48 -0500

    basic app

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, render_template, g, redirect, Response
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    print(request.args)
+
+    return 'Hello, World!'
+
+
+if __name__ == "__main__":
+  import click
+
+  @click.command()
+  @click.option('--debug', is_flag=True)
+  @click.option('--threaded', is_flag=True)
+  @click.argument('HOST', default='0.0.0.0')
+  @click.argument('PORT', default=8111, type=int)
+  def run(debug, threaded, host, port):
+    """
+    This function handles command line parameters.
+    Run the server using:
+
+        python3 server.py
+
+    Show the help text using:
+
+        python3 server.py --help
+
+    """
+
+    HOST, PORT = host, port
+    print("running on %s:%d" % (HOST, PORT))
+    app.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
+
+  run()
