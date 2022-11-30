bc9da93b316c81ce6ba98d38b3bcd121300bee5c
lievan <0evanli0@gmail.com>
2022-11-29 19:26:50 -0500

    added fake applicant data

--- app.py
+++ app.py
@@ -1,38 +1,23 @@
-from flask import Flask, request, render_template, g, redirect, Response
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+applicantData = [
+    {'name':'Panda Bear', 'school':'CC', 'grad':'2025', 'status':'Accepted'},
+    {'name':'Ice Bear', 'school':'SEAS', 'grad':'2024', 'status':'In Review'},
+    {'name':'Grizz Bear', 'school':'SEAS', 'grad':'2026', 'status':'Accepted'},
+    {'name':'Winnie T. Pooh', 'school':'GS', 'grad':'2023', 'status':'Rejected'},
+    {'name':'Kung Fu Panda', 'school':'CC', 'grad':'2024', 'status':'Accepted'},
+    {'name':'Paddington Bear', 'school':'BC', 'grad':'2025', 'status':'In Review'},
+]
+
+@app.route('/dashboard')
+def dash():
+    return render_template('dashboard.html', data = applicantData)
 
 @app.route('/')
-def index():
-    print(request.args)
-
+def hello_world():
     return 'Hello, World!'
 
-
-if __name__ == "__main__":
-  import click
-
-  @click.command()
-  @click.option('--debug', is_flag=True)
-  @click.option('--threaded', is_flag=True)
-  @click.argument('HOST', default='0.0.0.0')
-  @click.argument('PORT', default=8111, type=int)
-  def run(debug, threaded, host, port):
-    """
-    This function handles command line parameters.
-    Run the server using:
-
-        python3 server.py
-
-    Show the help text using:
-
-        python3 server.py --help
-
-    """
-
-    HOST, PORT = host, port
-    print("running on %s:%d" % (HOST, PORT))
-    app.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
-
-  run()
+if __name__ == '__main__':
+    app.run(debug = True)
